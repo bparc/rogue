34976bd74298f433ac2ec9ae5fec93a756e1bbe3
[milo] <slepowronska02@gmail.com>
2024-10-10 22:11:28 +0200

    enemies cant move to oob/walls

--- src/turn_based.c
+++ src/turn_based.c
@@ -12,9 +12,36 @@ fn s32 BeginTurn(game_world_t *World, entity_t *requestee)
 	return action_point_count;
 }
 
+//make sure enity cant move into wall/does nothing when trapped
 fn s32 Decide(game_world_t *World, entity_t *requestee)
 {
-	s32 cost = 1;
-	MoveEntity(World->map, requestee, cardinal_directions[rand() % 4]);
+	int randomIndex = rand() % 4;
+	v2s chosenDir = cardinal_directions[randomIndex]; //NOTE(): replaced by enemy ai
+	
+	int cantMove = false;
+	int attempts = 0;
+
+	v2s peekPos = AddS(requestee -> p, chosenDir);;
+	s32 cost = 0;
+
+	while(!cantMove && attempts < 5){
+		
+		cantMove = (!IsOutOfBounds(World, peekPos) && !IsWall(World, peekPos));
+		
+		if(cantMove) {
+			cost = 1; //only costs when can be moved
+			MoveEntity(World->map, requestee, chosenDir);
+			printf("moving on %d attempts", attempts);
+			break;
+		}
+
+		peekPos = AddS(requestee -> p, chosenDir);
+		chosenDir = cardinal_directions[(randomIndex + 1 ) % 4];
+		attempts++;
+		
+	}
+
+	
+	
 	return (cost);
 }
