334eae86b89f016347e1a03bb79d4b4d8854c659
Bartłomiej Pietrzyk <bartlomiejpiet97@gmail.com>
2024-10-12 12:37:30 +0200

    Moved TurnKernel() to turn_system.c

---
+++ src/turn_system.c
@@ -0,0 +1,143 @@
+fn void TurnKernel(game_world_t *state, entity_storage_t *storage, map_t *map, turn_queue_t *turns, f32 dt, client_input_t *input, virtual_controls_t cons, log_t *log, command_buffer_t *out)
+{
+	SetGlobalOffset(out, state->camera_position); // NOTE(): Let's pass the camera position via the PushRenderOutput call instead of this SetGlobalOffset stuff.
+	// NOTE(): Process the current turn
+	entity_t *entity = NextInOrder(turns, storage);
+	if (entity == 0)
+	{
+		// NOTE(): We run out of the turns, time to schedule new ones.
+		EstablishTurnOrder(state, turns, storage);
+	}
+	if (entity)
+	{
+		if ((turns->turn_inited == false))
+		{
+			turns->action_points = (BeginTurn(state, entity) + 1);
+			turns->turn_inited = true;
+
+			turns->interp_state = interp_request;
+			turns->time = 0.0f;
+			turns->time_elapsed = 0.0f;
+
+			#if ENABLE_TURN_SYSTEM_DEBUG_LOGS
+			LogLn(log, "TurnKernel(): initiating turn for entity#%i", entity->id);
+			#endif
+			// NOTE(): Begin the turn.
+		}
+
+		Assert(turns->turn_inited);
+		// NOTE(): The turn will "stall" until AcceptTurn() is called.
+
+		// NOTE(): DEBUG draw the entity's"discrete" p.
+		#if _DEBUG
+		RenderIsoTile(out, map, entity->p, Red(), false, 0);
+		#endif
+
+		state->camera_position = CameraTracking(state->camera_position, entity->deferred_p, GetViewport(input), dt);
+		
+		if (entity->flags & entity_flags_controllable) // NOTE(): The "player" code.
+		{
+			// NOTE(): Listen for the player input.
+			const v2s *considered_dirs = cardinal_directions;
+			#if ENABLE_DIAGONAL_MOVEMENT
+			if (IsKeyPressed(input, key_code_shift))
+				considered_dirs = diagonal_directions;
+			#endif
+			
+			s32 direction = GetDirectionalInput(input);
+			b32 input_valid = (direction >= 0) && (direction < 4);
+			b32 cursor_mode_active = state->cursor->active; // NOTE(): The cursor_active flag needs to be stored *before* calling DoCursor. This is actually the correct order. For reasons.
+
+			DoCursor(out, entity, cons, input_valid,
+				direction, considered_dirs, turns, map, storage, log, state->cursor);
+			
+			#if _DEBUG // NOTE(): Render the considered directions on the map.
+			v2s base_p = cursor_mode_active ? state->cursor->p : entity->p;
+			for (s32 index = 0; index < 4; index++)
+				RenderIsoTile(out, map, AddS(base_p, considered_dirs[index]), SetAlpha(Orange(), 0.5f), true, 0);
+			#endif
+			
+			//Valid input
+			if (input_valid && (cursor_mode_active == false))
+			{
+				//future position
+				v2s peekPos = AddS(entity -> p, considered_dirs[direction]);
+
+				//valid move pos
+				if(!IsOutOfBounds(state, peekPos) && !IsWall(state, peekPos))
+				{
+					if (turns->action_points > 0)
+						MoveEntity(map, entity, considered_dirs[direction]);
+
+					// NOTE(): Consume moves
+					turns->action_points--;
+					if (turns->action_points <= 0)
+						AcceptTurn(turns);
+				}
+			}
+		}
+		else
+		{
+			// NOTE(): Animator
+			f32 speed_mul = TURN_SPEED_NORMAL;
+			turns->time += dt * speed_mul;
+			turns->time_elapsed += dt;
+
+			switch(turns->interp_state)
+			{
+			case interp_request:
+				{
+					// TODO(): Move the player code somewhere around here maybe?
+					// It could potentially be a better way to structure this.
+					turns->starting_p = entity->p;
+
+					s32 cost = Decide(state, entity);
+						turns->action_points -= cost;
+					
+					turns->interp_state = interp_transit;
+					turns->time = 0.0f;
+				} break;
+			case interp_transit:
+				{
+					v2 a = GetTileCenter(map, turns->starting_p);
+					v2 b = GetTileCenter(map, entity->p);
+					entity->deferred_p = Lerp2(a, b, turns->time);
+					if ((turns->time >= 1.0f))
+					 {
+					 	if (turns->action_points > 0)
+					 	{
+					 		turns->interp_state = interp_request;
+					 	}
+					 	else
+					 	{
+					 		s32 attempt = AttemptAttack(state, entity);
+							turns->interp_state = attempt ? interp_attack : interp_accept;	
+					 	}
+						
+						turns->time = 0.0f;
+					}
+				} break;
+			case interp_attack:
+				{
+					DebugText(Add(ScreenToIso(entity->deferred_p), V2(120.0f, 60.0f)), "(I AM ATTACKING RIGHT NOW)");
+					if ((turns->time >= 2.0f))
+					{
+						turns->interp_state = interp_accept;
+						turns->time = 0.0f;
+					}
+				} break;
+			case interp_accept:
+				{
+					if (turns->time > 0.1f)
+					{
+						AcceptTurn(turns);
+
+						#ifdef ENABLE_TURN_SYSTEM_DEBUG_LOGS
+						LogLn(log, "TurnKernel(): turn finished in %.2f seconds", turns->time_elapsed);
+						#endif
+					}
+				} break;
+			}
+		}
+	}
+}
