8acff15a1ed21e6df1c1ce8e55c7b4bfbf6e6396
[milo] <slepowronska02@gmail.com>
2024-10-12 12:33:36 +0200

    linux build stuff

--- build.sh
+++ build.sh
@@ -11,17 +11,19 @@ gcc -o "x64/$DEBUG_OUTPUT/debug-x64" src/main_glfw.c -g -D_DEBUG -W -Wall -Wextr
 
 if [ $? -eq 0 ]; then
     echo "Debug build succeeded. Running..."
+    ."/x64/$DEBUG_OUTPUT/debug-x64"
 else
     echo "Debug build failed."
 fi
 
-# Release build
-gcc -o "x64/$RELEASE_OUTPUT/release-x64" src/main_glfw.c -O2 -D_RELEASE \
-    -W -Wall -Wextra -Wno-unused-variable -Wno-unreachable-code -Wno-unused-parameter \
-    -Wno-unused-function -Wno-sign-compare -lglfw -lGL -lm
 
-if [ $? -eq 0 ]; then
-    echo "Release build succeeded. Running..."
-else
-    echo "Release build failed."
-fi
+# Release build
+#gcc -o "x64/$RELEASE_OUTPUT/release-x64" src/main_glfw.c -O2 -D_RELEASE \
+ #   -W -Wall -Wextra -Wno-unused-variable -Wno-unreachable-code -Wno-unused-parameter \
+#    -Wno-unused-function -Wno-sign-compare -lglfw -lGL -lm
+
+#if [ $? -eq 0 ]; then
+ #   echo "Release build succeeded. Running..."
+#else
+ #   echo "Release build failed."
+#fi
