3b46bcdce85b58760f62bbde5e23fa31a75399b2
Bartłomiej Pietrzyk <bartlomiejpiet97@gmail.com>
2024-10-16 08:20:27 +0200

    Update turn_system.c

--- src/turn_system.c
+++ src/turn_system.c
@@ -41,7 +41,7 @@ fn inline s32 ChangeQueueState(turn_queue_t *queue, interpolator_state_t state)
 	return result;
 }
 
-fn void TurnKernel(game_world_t *state, entity_storage_t *storage, map_t *map, turn_queue_t *turns, f32 dt, client_input_t *input, virtual_controls_t cons, log_t *log, command_buffer_t *out)
+fn void TurnKernel(game_world_t *state, entity_storage_t *storage, map_t *map, turn_queue_t *queue, f32 dt, client_input_t *input, virtual_controls_t cons, log_t *log, command_buffer_t *out)
 {
 	SetGlobalOffset(out, state->camera_position); // NOTE(): Let's pass the camera position via the PushRenderOutput call instead of this SetGlobalOffset stuff.
 	// NOTE(): Process the current turn
@@ -50,22 +50,22 @@ fn void TurnKernel(game_world_t *state, entity_storage_t *storage, map_t *map, t
 	// the queue! This has to be fixed when we add the ability to
 	// remove an entity from the storage.
 
-	entity_t *entity = NextInOrder(turns, storage);
+	entity_t *entity = NextInOrder(queue, storage);
 	if (entity == 0)
 	{
-		// NOTE(): We run out of the turns, time to schedule new ones.
-		EstablishTurnOrder(state, turns, storage);
+		// NOTE(): We run out of the queue, time to schedule new ones.
+		EstablishTurnOrder(state, queue, storage);
 	}
 	if (entity)
 	{
-		if ((turns->turn_inited == false))
+		if ((queue->turn_inited == false))
 		{
-			turns->action_points = BeginTurn(state, entity);
-			turns->turn_inited = true;
+			queue->action_points = BeginTurn(state, entity);
+			queue->turn_inited = true;
 
-			turns->interp_state = interp_request;
-			turns->time = 0.0f;
-			turns->time_elapsed = 0.0f;
+			queue->interp_state = interp_request;
+			queue->time = 0.0f;
+			queue->time_elapsed = 0.0f;
 
 			#if ENABLE_TURN_SYSTEM_DEBUG_LOGS
 			DebugLog("initiating turn for entity#%i", entity->id);
@@ -73,7 +73,7 @@ fn void TurnKernel(game_world_t *state, entity_storage_t *storage, map_t *map, t
 			// NOTE(): Begin the turn.
 		}
 
-		Assert(turns->turn_inited);
+		Assert(queue->turn_inited);
 		// NOTE(): The turn will "stall" until AcceptTurn() is called.
 
 		// NOTE(): DEBUG draw the entity's"discrete" p.
@@ -100,7 +100,7 @@ fn void TurnKernel(game_world_t *state, entity_storage_t *storage, map_t *map, t
 			b32 cursor_mode_active = state->cursor->active; // NOTE(): The cursor_active flag needs to be stored *before* calling DoCursor. This is actually the correct order. For reasons.
 
 			DoCursor(out, entity, cons, input_valid,
-				direction, considered_dirs, turns, map, storage, log, state->cursor);
+				direction, considered_dirs, queue, map, storage, log, state->cursor);
 			
 			#if _DEBUG // NOTE(): Render the considered directions on the map.
 			v2s base_p = cursor_mode_active ? state->cursor->p : entity->p;
@@ -115,34 +115,34 @@ fn void TurnKernel(game_world_t *state, entity_storage_t *storage, map_t *map, t
 				v2s peekPos = AddS(entity -> p, considered_dirs[direction]);
 
 				//valid move pos
-				if (IsWorldPointEmpty(state, peekPos) && (turns->action_points > 0))
+				if (IsWorldPointEmpty(state, peekPos) && (queue->action_points > 0))
 				{
 			        MoveEntity(map, entity, considered_dirs[direction]);
 			        ApplyTileEffects(peekPos, state, entity);
 					// NOTE(): Consume moves
-					turns->action_points--;
+					queue->action_points--;
 				}
 			}
 
 			// NOTE(): End the turn if we run out of all action points.
-			if (turns->action_points <= 0)
-						AcceptTurn(turns);
+			if (queue->action_points <= 0)
+						AcceptTurn(queue);
 		}
 		else
 		{
 			// NOTE(): Animator
 			f32 speed_mul = TURN_SPEED_NORMAL;
-			turns->time += dt * speed_mul;
-			turns->time_elapsed += dt;
+			queue->time += dt * speed_mul;
+			queue->time_elapsed += dt;
 
-			switch(turns->interp_state)
+			switch(queue->interp_state)
 			{
 			case interp_wait_for_input:
 				{
-					if (turns->request_step)
+					if (queue->request_step)
 					{
-						turns->interp_state = turns->requested_step;
-						turns->time = 0.0f;
+						queue->interp_state = queue->requested_step;
+						queue->time = 0.0f;
 					}
 				} break;
 			Request:
@@ -150,48 +150,48 @@ fn void TurnKernel(game_world_t *state, entity_storage_t *storage, map_t *map, t
 				{
 					// TODO(): Move the player code somewhere around here maybe?
 					// It could potentially be a better way to structure this.
-					turns->starting_p = entity->p;
+					queue->starting_p = entity->p;
 
 					s32 cost = Decide(state, entity);
-						turns->action_points -= cost;
+						queue->action_points -= cost;
 					
-					if (!ChangeQueueState(turns, interp_transit))
+					if (!ChangeQueueState(queue, interp_transit))
 						break;
-					turns->time = dt;
+					queue->time = dt;
 				}; // NOTE(): Intentional fall-through. We want to start handling transit immediatly, instead of waiting an addidional frame.
 			case interp_transit:
 				{
-					v2 a = GetTileCenter(map, turns->starting_p);
+					v2 a = GetTileCenter(map, queue->starting_p);
 					v2 b = GetTileCenter(map, entity->p);
-					entity->deferred_p = Lerp2(a, b, turns->time);
-					if ((turns->time >= 1.0f))
+					entity->deferred_p = Lerp2(a, b, queue->time);
+					if ((queue->time >= 1.0f))
 					{
-						turns->time = 0.0f;
+						queue->time = 0.0f;
 
-					 	if (turns->action_points > 0)
+					 	if (queue->action_points > 0)
 					 	{
-					 		turns->interp_state = interp_request;
-					 		goto Request;
+					 		if (ChangeQueueState(queue, interp_request))
+					 			goto Request;
 					 	}
 					 	else
 					 	{
 					 		s32 attempt = AttemptAttack(state, entity);
-							ChangeQueueState(turns, attempt ? interp_attack : interp_accept);	
+							ChangeQueueState(queue, attempt ? interp_attack : interp_accept);	
 					 	}
 					}
 				} break;
 			case interp_attack:
 				{
-					if ((turns->time >= 0.5f))
-						ChangeQueueState(turns, interp_accept);
+					if ((queue->time >= 0.5f))
+						ChangeQueueState(queue, interp_accept);
 				} break;
 			case interp_accept:
 				{
-					if (turns->time > 0.1f)
+					if (queue->time > 0.1f)
 					{
-						AcceptTurn(turns);
+						AcceptTurn(queue);
 						#ifdef ENABLE_TURN_SYSTEM_DEBUG_LOGS
-						DebugLog("TurnKernel(): turn finished in %.2f seconds", turns->time_elapsed);
+						DebugLog("TurnKernel(): turn finished in %.2f seconds", queue->time_elapsed);
 						#endif
 					}
 				} break;
